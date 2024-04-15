4be3d4277247e2fc463729ccc2f596b245db9db7
Rena <150563337+xenoroxy@users.noreply.github.com>
2024-04-15 16:59:00 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,36 +1,37 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
-public class Question5 
-{
-    public static void main(String[] args) 
-  {
-        Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        int[] numbers = new int[num];
-      
-        for (int i = 0; i < num;) {
-            System.out.println("Enter integer " + (i + 1) + ":");
-            numbers[i] = scanner.nextInt();
+public class Question5 {
+    public static void main(String[] args) {
+        /**
+         * Prompt the user for number. This input indicates the number of integers the user will be entering next.
+         * Print out the mode (highest occurrence) from the set of integers.
+         * e.g. > 5 > 2 > 4 > 1 > 3 > 4 4
+         * e.g. > 4 > 2 > 2 > 3 > 3 2
+         * Hint: Use a loop to get input. Use another 2 loops to find the mode
+         */
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        HashMap<Integer, Integer> countMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
 
         int mode = 0;
         int maxCount = 0;
 
-        // Find the mode (integer with the highest occurrence)
-        for (int i = 0; i < num;) {
-            int count = 0;
-            for (int j = 0; j < num; j++) {
-                if (numbers[j] == numbers[i]) {
-                    count++;
-                }
-            }
+        for (int num : countMap.keySet()) {
+            int count = countMap.get(num);
             if (count > maxCount) {
-                mode = numbers[i];
+                mode = num;
                 maxCount = count;
             }
         }
 
-        // Print the mode
-        System.out.println("Mode: " + mode);
+        System.out.println(mode);
     }
 }
